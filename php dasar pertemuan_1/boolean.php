7db80d4db40061c34efebda2c009e39fbc3e910d
Syifa Maimunah <syifamaimunah52@gmail.com>
2026-07-31 08:53:42 +0700

    tugas_php_pertemuan_1

---
+++ php dasar pertemuan_1/boolean.php
@@ -0,0 +1,11 @@
+<?php
+// Tipe Data Boolean (True/False)
+echo "benar: ";
+echo true; 
+echo "<br>";
+
+echo "salah: ";
+echo false; 
+echo "<br><hr>";
+
+?>
---
+++ php dasar pertemuan_1/number.php
@@ -0,0 +1,12 @@
+<?php
+// Tipe Data Number (Integer & Float)
+$umur = 20;           
+$nilai = 89.5;        
+
+echo 'Umur (Integer) : ';
+echo $umur;
+echo '<br>';
+
+echo 'Nilai (Float) : ';
+echo $nilai;
+?>
